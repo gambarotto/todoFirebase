a8b87b08bab092e789394574e33f068aa1fcad68
diego <dgcdeveloper@gmail.com>
2019-07-18 17:37:17 -0300

    initial version

---
+++ src/Main/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { StyleSheet, Platform, Image, Text, View, ScrollView } from 'react-native';
+
+import firebase from 'react-native-firebase';
+
+export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  async componentDidMount() {
+    // TODO: You: Do firebase things
+    // const { user } = await firebase.auth().signInAnonymously();
+    // console.warn('User -> ', user.toJSON());
+
+    // await firebase.analytics().logEvent('foo', { bar: '123'});
+  }
+
+  render() {
+    return (
+      <View>
+        <Text>ola</Text>
+      </View>
+    );
+  }
+}
